5342014e10441d013605c008f2dc71c84ab1a8ae
gregBifes <gregbifes@gmail.com>
2025-08-24 14:27:53 +0200

    dayJs libray, delivery options

--- data/products.js
+++ data/products.js
@@ -1,3 +1,26 @@
+import dayjs from 'dayjs';
+const today = dayjs();
+console.log(today);
+export const deliveryOptions = [
+    {
+        id: '1',
+        deliveryTime: dayjs(''),
+        deliveryPriceCents: ''
+    },
+    {
+        id: '2',
+        deliveryTime: '',
+        deliveryPriceCents: ''
+    },
+    {
+        id: '3',
+        deliveryTime: '',
+        deliveryPriceCents: ''
+    }
+
+]
+
+
 export const products = [
     {
         id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
